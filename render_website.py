41a3dacb912d70407cc692a8d12a6d54ddf46764
GenmaHimmuro <t.suleimanov2@gmail.com>
2025-06-27 14:43:33 +0400

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -2,17 +2,24 @@ import json
 from pprint import pprint
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from livereload import Server, shell
 
 
-with open("meta_data.json", "r", encoding='UTF-8') as my_file:
-    meta_data_json = my_file.read()
+def render_library_site():
+    with open("meta_data.json", "r", encoding='UTF-8') as my_file:
+        meta_data_json = my_file.read()
 
-books = json.loads(meta_data_json)
-pprint(books)
+    books = json.loads(meta_data_json)
 
-env = Environment(loader=FileSystemLoader('.'), autoescape=select_autoescape(['html', 'xml']))
-template = env.get_template('template.html')
-rendered_page = template.render(books=books)
+    env = Environment(loader=FileSystemLoader('.'), autoescape=select_autoescape(['html', 'xml']))
+    template = env.get_template('template.html')
+    rendered_page = template.render(books=books)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+render_library_site()
+
+server = Server()
+server.watch('template.html', render_library_site)
+server.serve(root='.')
