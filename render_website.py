b1cf8f5be1815d7e7261402860695f0ecdccc742
GenmaHimmuro <t.suleimanov2@gmail.com>
2025-06-27 14:28:59 +0400

    Create HTML page with books

---
+++ render_website.py
@@ -0,0 +1,18 @@
+import json
+from pprint import pprint
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+
+with open("meta_data.json", "r", encoding='UTF-8') as my_file:
+    meta_data_json = my_file.read()
+
+books = json.loads(meta_data_json)
+pprint(books)
+
+env = Environment(loader=FileSystemLoader('.'), autoescape=select_autoescape(['html', 'xml']))
+template = env.get_template('template.html')
+rendered_page = template.render(books=books)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
